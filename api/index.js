dbce523b02769cd32bdd0c29b6b9b69278ed787c
Jonathan de BOISVILLIERS <jdeboisvilliers@gmail.com>
2021-08-18 08:55:35 +0200

    add readme git flow

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 
+app.listen(3000)
+
 const app = express();
 
 const port = process.env.PORT || 3500;
 
-app.listen(port, _ => {console.log(`http://localhost:port`)});
+app.listen(port, _ => { console.log(`http://localhost:port`) });
