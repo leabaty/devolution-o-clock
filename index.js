e2160b0eff18be7ee9b04fc37b6fbbdaf31f1f0c
Badara-Seydi <badara.seydi.dev@gmail.com>
2021-08-20 23:57:02 +0200

    try to fix front(database et index.js en trop)

--- index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const express = require('express');
-
-const app = express();
-
-const port = process.env.PORT || 3500;
-
-app.listen(port, _ => {console.log('http://localhost:3500')});
