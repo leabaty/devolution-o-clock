2f8c8f36ad28fd9ea74a9841d4078e5547a1093f
Badara-Seydi <badara.seydi.dev@gmail.com>
2021-08-20 16:46:54 +0200

    CRUD user/project OK

--- api/app/routers/index.js
+++ api/app/routers/index.js
@@ -3,13 +3,37 @@ const express = require('express');
 const router = express.Router();
 
 
-const {mainController,userController} = require('../controllers');
+const {mainController,userController, projectController} = require('../controllers');
 
+
+
+//User
 router.get('/',mainController.homePage);
-router.get('/login', userController.login);
-router.post('/login', userController.login);
-router.post('/create' , userController.signUp);
-router.get('/search/:query', mainController.searchProject);
+router.get('/login',userController.login);
+router.post('/login',userController.login);
+router.put('/user/:id',userController.updateOne);
+router.post('/user/create' ,userController.signUp);
+router.get('/users' ,userController.getAll);
+router.get('/user/:id' ,userController.getOne);
+router.delete('/user/:id' ,userController.deleteOne);
+
+//Projects
+router.get('/projects',projectController.getAll);
+router.post('/project/create', projectController.create)
+router.get('/project/:id' ,projectController.getOne);
+router.put('/project/:id',projectController.updateOne);
+router.delete('/project/:id' ,projectController.deleteOne);
+
+
+router.get('/search/:query',mainController.searchProject);
+router.post('/projects/:search',projectController.search);
+
+
+
+
+
+
+
 
 module.exports=router
 
